56cce6c5742f6a43ac269e42da8c46b2eaa8b3c7
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-06-18 21:37:48 +0200

    Poprawki

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { PostComponent } from './post/post.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  AfterViewInit{
   title = 'Paul Angular';
   parentMessage:string = 'Message coming from parent component'
+  message!: string;
+  @ViewChild(PostComponent)
+  childComp!: { childMessage: string; };
+  constructor(){
+    console.log(this.childComp);
+  }
+  ngAfterViewInit(): void {
+      console.log(this.childComp);
+      this.message = this.childComp.childMessage;
+  }
+
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -10,6 +10,7 @@ export class PostComponent implements OnInit {
   messagePost: string = 'Message Post';
 
 postParentMessage:string = 'Message coming from the post parent'
+childMessage:string = 'From Child Component';
   @Input() fromParent:string ='';
 
 constructor(){}
